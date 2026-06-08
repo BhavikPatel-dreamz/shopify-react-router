1ecc941b1f99e3e1fd5e860736001c4293717237
Dynamic <support@dynamicdreamz.com>
2026-06-08 16:16:24 +0530

    added megamenu data

--- app/data/megaMenuData.ts
+++ app/data/megaMenuData.ts
@@ -137,5 +137,259 @@ export const megaMenuData = {
         },
     ],
 
-   
+    women: [
+        {
+            title: "NEW IN",
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER_-_DESTKTOP_-_new_in.webp?v=1779175946",
+            imageLink: "/women/new-in",
+            type: "slider",
+        },
+
+        {
+            title: "THE EDIT",
+            links: [
+                { title: "Denim", url: "/women/denim" },
+                { title: "This Week", url: "/women/this-week" },
+                { title: "Top Mode On", url: "/women/top-mode-on" },
+                { title: "Summer Tales", url: "/women/summer-tales" },
+                { title: "Ruby", url: "/women/ruby" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-THE_EDIT.webp?v=1779271946",
+            imageLink: "/women/new-arrivals",
+        },
+
+        {
+            title: "DENIM",
+            links: [
+                { title: "View All", url: "/women/denim" },
+                { title: "Jeans", url: "/women/denim" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-DENIM.webp?v=11441343761345001647",
+            imageLink: "/women/denim",
+        },
+
+        {
+            title: "CLOTHING",
+            links: [
+                { title: "View All", url: "/women/all-products" },
+                { title: "Best Sellers", url: "/women/best-sellers" },
+                { title: "Dress", url: "/women/dress" },
+                { title: "Tops", url: "/women/tops" },
+                { title: "Shirts", url: "/women/shirts" },
+                { title: "T-Shirts", url: "/women/t-shirt" },
+                { title: "Polos", url: "/women/polo" },
+                { title: "Co-ords", url: "/women/co-ords" },
+                { title: "Trouser", url: "/women/trouser" },
+                { title: "Jeans", url: "/women/denim" },
+                { title: "Skirts", url: "/women/skirt" },
+                { title: "Shorts", url: "/women/short" },
+                { title: "Blazer", url: "/women/blazer" },
+                { title: "Trackpants", url: "/women/track-pant" },
+                { title: "Sweater", url: "/women/sweater" },
+                { title: "Cardigan | Shrug", url: "/women/shrug-overlay" },
+                { title: "Sweatshirt", url: "/women/sweatshirt" },
+                { title: "Jackets", url: "/women/jackets" },
+                { title: "Coats", url: "/women/coat" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-ALL_PRODUCTS.webp?v=1779271859",
+            imageLink: "/women/all-products",
+        },
+
+        {
+            title: "SALE",
+            links: [
+                { title: "View All", url: "/women/sale" },
+                { title: "Clothing", url: "/women/sale/clothing" },
+                { title: "Shoes", url: "/women/sale/shoes" },
+                { title: "Accessories", url: "/women/sale/accessories" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER_-_DESTKTOP_-_Sale_-RSM.webp?v=1760444150",
+            imageLink: "/women/sale",
+        },
+
+        {
+            title: "WORK WEAR",
+            links: [
+                { title: "View All", url: "/women/work-wear" },
+                { title: "Dress", url: "/women/work-wear/dress" },
+                { title: "Tops", url: "/women/work-wear/tops" },
+                { title: "Polo", url: "/women/work-wear/polo" },
+                { title: "Shirts", url: "/women/work-wear/shirts" },
+                { title: "Trousers", url: "/women/work-wear/trousers" },
+                { title: "Blazer", url: "/women/work-wear/blazer" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-WORK_WEAR.webp?v=1779271819",
+            imageLink: "/women/work-wear",
+        },
+
+        {
+            title: "ACCESSORIES",
+            links: [
+                { title: "Belts", url: "/women/accessories/belts" },
+                { title: "Scarfs", url: "/women/accessories/scarfs" },
+                { title: "Jewellery", url: "/women/accessories/jewellery" },
+                { title: "Bags", url: "/women/accessories/bags" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-ACCESSORIES.webp?v=1779272156",
+            imageLink: "/women/accessories",
+        },
+
+        {
+            title: "COLLABORATIONS",
+            links: [
+                { title: "RAREISM x BOBO", url: "/pages/rareism-bobo" },
+            ],
+        },
+
+        {
+            title: "GIFT CARD",
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-GIFT_CARD.webp?v=1840812756811755102",
+            imageLink: "/collections/gift-card-1",
+        },
+    ],
+
+    kids: [
+        {
+            title: "NEW IN",
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER_-_DESTKTOP_-_new_in.webp?v=1779175946",
+            imageLink: "/kids/new-in",
+            type: "slider",
+        },
+
+        {
+            title: "ALL PRODUCTS",
+            links: [
+                { title: "View All", url: "/kids/all-products" },
+                { title: "Best Sellers", url: "/kids/best-sellers" },
+                { title: "Tops", url: "/kids/tops" },
+                { title: "T-Shirts", url: "/kids/t-shirts" },
+                { title: "Shirts", url: "/kids/shirts" },
+                { title: "Shorts", url: "/kids/shorts" },
+                { title: "Trousers", url: "/kids/trousers" },
+                { title: "Jeans", url: "/kids/jeans" },
+                { title: "Dresses", url: "/kids/dresses" },
+                { title: "Skirts", url: "/kids/skirts" },
+                { title: "Jackets", url: "/kids/jackets" },
+                { title: "Sweaters", url: "/kids/sweaters" },
+                { title: "Hoodies", url: "/kids/hoodies" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-ALL_PRODUCTS.webp?v=1779271859",
+            imageLink: "/kids/all-products",
+        },
+
+        {
+            title: "CLOTHING",
+            links: [
+                { title: "Boys", url: "/kids/boys" },
+                { title: "Girls", url: "/kids/girls" },
+                { title: "Unisex", url: "/kids/unisex" },
+            ],
+        },
+
+        {
+            title: "ACCESSORIES",
+            links: [
+                { title: "Bags", url: "/kids/accessories/bags" },
+                { title: "Hats", url: "/kids/accessories/hats" },
+                { title: "Scarves", url: "/kids/accessories/scarves" },
+            ],
+        },
+
+        {
+            title: "SALE",
+            links: [
+                { title: "View All", url: "/kids/sale" },
+                { title: "Clothing", url: "/kids/sale/clothing" },
+                { title: "Shoes", url: "/kids/sale/shoes" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER_-_DESTKTOP_-_Sale_-RSM.webp?v=1760444150",
+            imageLink: "/kids/sale",
+        },
+
+        {
+            title: "GIFT CARD",
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-GIFT_CARD.webp?v=1840812756811755102",
+            imageLink: "/collections/gift-card-1",
+        },
+    ],
+
+    shoes: [
+        {
+            title: "NEW IN",
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER_-_DESTKTOP_-_new_in.webp?v=1779175946",
+            imageLink: "/shoes/new-in",
+            type: "slider",
+        },
+
+        {
+            title: "ALL SHOES",
+            links: [
+                { title: "View All", url: "/shoes" },
+                { title: "Best Sellers", url: "/shoes/best-sellers" },
+                { title: "Sneakers", url: "/shoes/sneakers" },
+                { title: "Loafers", url: "/shoes/loafers" },
+                { title: "Boots", url: "/shoes/boots" },
+                { title: "Sandals", url: "/shoes/sandals" },
+                { title: "Casual", url: "/shoes/casual" },
+                { title: "Formal", url: "/shoes/formal" },
+                { title: "Sports", url: "/shoes/sports" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-ALL_PRODUCTS.webp?v=1779271859",
+            imageLink: "/shoes",
+        },
+
+        {
+            title: "BY STYLE",
+            links: [
+                { title: "Sneakers", url: "/shoes/sneakers" },
+                { title: "Loafers", url: "/shoes/loafers" },
+                { title: "Boots", url: "/shoes/boots" },
+                { title: "Sandals", url: "/shoes/sandals" },
+                { title: "Dress Shoes", url: "/shoes/formal" },
+            ],
+        },
+
+        {
+            title: "BY COLLECTION",
+            links: [
+                { title: "Men", url: "/men/shoes" },
+                { title: "Women", url: "/women/shoes" },
+                { title: "Kids", url: "/kids/shoes" },
+            ],
+        },
+
+        {
+            title: "SALE",
+            links: [
+                { title: "View All", url: "/shoes/sale" },
+                { title: "Men", url: "/shoes/sale/men" },
+                { title: "Women", url: "/shoes/sale/women" },
+                { title: "Kids", url: "/shoes/sale/kids" },
+            ],
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER_-_DESTKTOP_-_Sale_-RSM.webp?v=1760444150",
+            imageLink: "/shoes/sale",
+        },
+
+        {
+            title: "GIFT CARD",
+            image:
+                "https://86e75ac3.nitro.getn7.io/cdn/shop/files/MENU_BANNER-RSM-MOBILE-GIFT_CARD.webp?v=1840812756811755102",
+            imageLink: "/collections/gift-card-1",
+        },
+    ],
 };
