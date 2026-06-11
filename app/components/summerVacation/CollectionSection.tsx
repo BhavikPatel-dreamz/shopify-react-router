47238137ef9bb7c2450a5337dea6dfb686ddde3c
Khushbutara <khushbutarashekh3@gmail.com>
2026-06-11 11:43:58 +0530

    component  deleted

--- app/components/summerVacation/CollectionSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CollectionSection() {
-    return (
-        <section className="collection-section-empty">
-            <div className="collection-empty-wrapper" />
-        </section>
-    );
-}
