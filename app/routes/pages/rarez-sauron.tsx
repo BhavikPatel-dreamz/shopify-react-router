ae16e0646010eab8b05c6f7d8c1751cd3437cbe6
Dynamic <support@dynamicdreamz.com>
2026-06-11 10:02:46 +0530

    fixed error

--- app/routes/pages/rarez-sauron.tsx
+++ app/routes/pages/rarez-sauron.tsx
@@ -1,17 +1,42 @@
-import React from "react";
+import { useLoaderData } from "react-router";
 
 import LandingVideo from "~/components/LandingVideo";
 import LandingImageSection from "~/components/LandingImageSection";
-
 import {
   rarezSauronImages,
   rarezSauronVideos,
 } from "~/data/rarezSauron";
 import CollectionGrid from "~/components/collection/CollectionGrid";
+import { COLLECTION_QUERY } from "~/graphQL/collection";
+import { createStorefrontClient } from "~/server/storefront.server";
+import type { Route } from "./+types/rarez-sauron";
+
+export async function loader({}: Route.LoaderArgs) {
+  const storefront = createStorefrontClient();
+
+  const data = await storefront.query<{
+    collection?: {
+      products?: {
+        nodes: any[];
+      };
+    };
+  }>(COLLECTION_QUERY, {
+    variables: {
+      handle: "rarez-sauron",
+      pageBy: 12,
+      country: "IN",
+      language: "EN",
+    },
+  });
+
+  return {
+    products: data.collection?.products?.nodes ?? [],
+  };
+}
 
 export default function RarezSauronPage() {
-  // Provide a fallback products array to avoid undefined variable error.
-  const products: any[] = [];
+  const { products } = useLoaderData<typeof loader>();
+
   return (
     <main className="rarez-sauron-page">
       {rarezSauronVideos.map((video, index) => (
